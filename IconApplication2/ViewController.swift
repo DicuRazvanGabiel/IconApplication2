cc190f622e0c9eb0afba060890733ac832c36918
razvan <razvan@staicus-macbook-air.local>
2018-09-13 11:43:10 +0300

    some function implemented

--- IconApplication2/ViewController.swift
+++ IconApplication2/ViewController.swift
@@ -56,12 +56,47 @@ class ViewController: NSViewController, NSCollectionViewDataSource, NSCollection
         }
         customView.wantsLayer = true
         customView.layer?.backgroundColor = NSColor(red: 1, green: 1, blue: 1, alpha: 0.5).cgColor
+        
+        imangesContainer.wantsLayer = true
+        
+        //setBackgroundColor(color: NSColor.red)
+        //roundBackgroundCorners(amount: 100.0)
+        //setBackGroundBorder(color: NSColor.red, thickness: 20)
+        //setBackGroundShadow(offset: 0, color: NSColor.black, blur: 10)
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+    
+    override func keyUp(with event: NSEvent) {
+        
+        //bail out if we dont have any selected items
+        guard collectionView.selectionIndexPaths.count > 0 else { return }
+        
+        //convert the integer to a Unicode scalar, then to a string
+        if event.charactersIgnoringModifiers == String(UnicodeScalar(NSDeleteCharacter)!) {
+            
+            let fm = FileManager.default
+            
+            //loop over the selected items in reverse sorted order
+            for indexPath in collectionView.selectionIndexPaths.sorted().reversed() {
+                
+                do {
+                    //move this item to the trash and remove it from the array
+                    try fm.trashItem(at: photos[indexPath.item].urlImage!, resultingItemURL: nil)
+                    photos.remove(at: indexPath.item)
+                    
+                } catch {
+                    print("Failed to delete |(photos[indexPath.item])")
+                }
+            }
+            //remove the items from the collection view
+            collectionView.animator().deleteItems(at: collectionView.selectionIndexPaths)
+        }
+        arrangementImagesToPreview()
     }
     
     @IBAction func onCheckBoxBorder(_ sender: Any) {
@@ -72,6 +107,38 @@ class ViewController: NSViewController, NSCollectionViewDataSource, NSCollection
             imageViewOfMovableImage?.layer?.borderWidth = 10
         }else{
             imageViewOfMovableImage?.layer?.borderWidth = 0
+        }
+    }
+    
+    @IBAction func changeToBackgorundView(_ sender: NSView){
+        tabViewController.selectTabViewItem(at: 0)
+    }
+    
+    @IBAction func changeToImageView(_ sender: NSView){
+        tabViewController.selectTabViewItem(at: 1)
+    }
+    
+    @IBAction func changeToTextView(_ sender: NSView){
+        tabViewController.selectTabViewItem(at: 2)
+    }
+    
+    @IBAction func exportImage(_ sender: NSView){
+        let imageToExport = generateImageToExport()
+        let image =  imageToExport
+        guard let tiffData = image.tiffRepresentation else { return }
+        guard let imageRep = NSBitmapImageRep(data: tiffData) else { return }
+        guard let png = imageRep.representation(using: .png, properties: [:]) else { return }
+        let panel = NSSavePanel()
+        panel.allowedFileTypes = ["png"]
+        panel.begin { result in
+            if result == NSApplication.ModalResponse.OK {
+                guard let url = panel.url else { return }
+                do {
+                    try png.write(to: url)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
         }
     }
 
@@ -211,34 +278,6 @@ class ViewController: NSViewController, NSCollectionViewDataSource, NSCollection
         return photos[indexPath.item].urlImage as NSPasteboardWriting?
     }
     
-    override func keyUp(with event: NSEvent) {
-        
-        //bail out if we dont have any selected items
-        guard collectionView.selectionIndexPaths.count > 0 else { return }
-        
-        //convert the integer to a Unicode scalar, then to a string
-        if event.charactersIgnoringModifiers == String(UnicodeScalar(NSDeleteCharacter)!) {
-            
-            let fm = FileManager.default
-            
-            //loop over the selected items in reverse sorted order
-            for indexPath in collectionView.selectionIndexPaths.sorted().reversed() {
-                
-                do {
-                    //move this item to the trash and remove it from the array
-                    try fm.trashItem(at: photos[indexPath.item].urlImage!, resultingItemURL: nil)
-                    photos.remove(at: indexPath.item)
-                    
-                } catch {
-                    print("Failed to delete |(photos[indexPath.item])")
-                }
-            }
-            //remove the items from the collection view
-            collectionView.animator().deleteItems(at: collectionView.selectionIndexPaths)
-        }
-        arrangementImagesToPreview()
-    }
-    
     func addMovableImageToPreview(movableImageToAdd: MovableImage, indexOfXY: Int){
         let imageImported = NSImage(byReferencing: movableImageToAdd.urlImage!)
         movableImageToAdd.frame = NSRect(x:indexOfXY * 10, y:indexOfXY * 10, width: 1, height: 1)
@@ -262,6 +301,10 @@ class ViewController: NSViewController, NSCollectionViewDataSource, NSCollection
         }
     }
     
+    func setBackGroundShadow(offset: Float = 0, color: NSColor = NSColor.blue, blur: Float = 5){
+        imangesContainer.shadow = setShadow(offset: offset, color: color, blur: blur)
+    }
+    
     func setShadow(offset: Float = 0, color: NSColor = NSColor.blue, blur: Float = 5) -> NSShadow{
         let shadow = NSShadow()
         shadow.shadowOffset = NSSize(width: CGFloat(offset), height: CGFloat(offset))
@@ -274,41 +317,9 @@ class ViewController: NSViewController, NSCollectionViewDataSource, NSCollection
     
     func setSelectedImage(selectImage: PhotoLayer){
         selectedImage = selectImage
+        scaleSelectedImage()
     }
-    
-    @IBAction func changeToBackgorundView(_ sender: NSView){
-        tabViewController.selectTabViewItem(at: 0)
-    }
-    
-    @IBAction func changeToImageView(_ sender: NSView){
-        tabViewController.selectTabViewItem(at: 1)
-    }
-    
-    @IBAction func changeToTextView(_ sender: NSView){
-        tabViewController.selectTabViewItem(at: 2)
-    }
-    
-    @IBAction func exportImage(_ sender: NSView){
-        let imageToExport = generateImageToExport()
-        let image =  imageToExport
-        guard let tiffData = image.tiffRepresentation else { return }
-        guard let imageRep = NSBitmapImageRep(data: tiffData) else { return }
-        guard let png = imageRep.representation(using: .png, properties: [:]) else { return }
-        let panel = NSSavePanel()
-        panel.allowedFileTypes = ["png"]
-        panel.begin { result in
-            if result == NSApplication.ModalResponse.OK {
-                guard let url = panel.url else { return }
-                do {
-                    try png.write(to: url)
-                } catch {
-                    print(error.localizedDescription)
-                }
-            }
-        }
-        
-    }
-    
+
     func generateImageToExport() -> NSImage{
         let image = NSImage(size: CGSize(width: 700, height: 720), flipped: false) { [unowned self] rect in
             
@@ -331,20 +342,25 @@ class ViewController: NSViewController, NSCollectionViewDataSource, NSCollection
             //image?.draw(in: rect, from: imageOrigin, operation: .sourceOver, fraction: 1)
         }
     }
+
+    func roundBackgroundCorners(amount: Float = 0.5){
+        imangesContainer.layer?.cornerRadius = CGFloat(amount)
+    }
     
+    func setBackgroundColor(color: NSColor){
+        imangesContainer.layer?.backgroundColor = color.cgColor
+    }
     
+    func setBackGroundBorder(color: NSColor, thickness: Float){
+        imangesContainer.layer?.borderColor = color.cgColor
+        imangesContainer.layer?.borderWidth = CGFloat(thickness)
+    }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    func scaleSelectedImage(amount: Float = 100){
+        let newSize = NSSize(width: 200, height: 200)
+        let newImage = selectedImage?.movableImage.image?.resizeWhileMaintainingAspectRatioToSize(size: newSize)
+        //selectedImage?.movableImage.image? = newImage!
+    }
 }
 
 extension Array {
@@ -359,5 +375,111 @@ extension Array {
         } else {
             self.insert(item, at: to - 1)
         }
+    }
+}
+
+extension NSImage {
+    
+    /// Returns the height of the current image.
+    var height: CGFloat {
+        return self.size.height
+    }
+    
+    /// Returns the width of the current image.
+    var width: CGFloat {
+        return self.size.width
+    }
+    
+    ///  Copies the current image and resizes it to the given size.
+    ///
+    ///  - parameter size: The size of the new image.
+    ///
+    ///  - returns: The resized copy of the given image.
+    func copy(size: NSSize) -> NSImage? {
+        // Create a new rect with given width and height
+        let frame = NSMakeRect(0, 0, size.width, size.height)
+        
+        // Get the best representation for the given size.
+        guard let rep = self.bestRepresentation(for: frame, context: nil, hints: nil) else {
+            return nil
+        }
+        
+        // Create an empty image with the given size.
+        let img = NSImage(size: size)
+        
+        // Set the drawing context and make sure to remove the focus before returning.
+        img.lockFocus()
+        defer { img.unlockFocus() }
+        
+        // Draw the new image
+        if rep.draw(in: frame) {
+            return img
+        }
+        
+        // Return nil in case something went wrong.
+        return nil
+    }
+    
+    ///  Copies the current image and resizes it to the size of the given NSSize, while
+    ///  maintaining the aspect ratio of the original image.
+    ///
+    ///  - parameter size: The size of the new image.
+    ///
+    ///  - returns: The resized copy of the given image.
+    func resizeWhileMaintainingAspectRatioToSize(size: NSSize) -> NSImage? {
+        let newSize: NSSize
+        
+        let widthRatio  = size.width / self.width
+        let heightRatio = size.height / self.height
+        
+        if widthRatio > heightRatio {
+            newSize = NSSize(width: floor(self.width * widthRatio), height: floor(self.height * widthRatio))
+        } else {
+            newSize = NSSize(width: floor(self.width * heightRatio), height: floor(self.height * heightRatio))
+        }
+        
+        return self.copy(size: newSize)
+    }
+    
+    ///  Copies and crops an image to the supplied size.
+    ///
+    ///  - parameter size: The size of the new image.
+    ///
+    ///  - returns: The cropped copy of the given image.
+    func crop(size: NSSize) -> NSImage? {
+        // Resize the current image, while preserving the aspect ratio.
+        guard let resized = self.resizeWhileMaintainingAspectRatioToSize(size: size) else {
+            return nil
+        }
+        // Get some points to center the cropping area.
+        let x = floor((resized.width - size.width) / 2)
+        let y = floor((resized.height - size.height) / 2)
+        
+        // Create the cropping frame.
+        let frame = NSMakeRect(x, y, size.width, size.height)
+        
+        // Get the best representation of the image for the given cropping frame.
+        guard let rep = resized.bestRepresentation(for: frame, context: nil, hints: nil) else {
+            return nil
+        }
+        
+        // Create a new image with the new size
+        let img = NSImage(size: size)
+        
+        img.lockFocus()
+        defer { img.unlockFocus() }
+        
+        if rep.draw(in: NSMakeRect(0, 0, size.width, size.height),
+                    from: frame,
+                    operation: NSCompositingOperation.copy,
+                    fraction: 1.0,
+                    respectFlipped: false,
+                    hints: [:]) {
+            // Return the cropped image.
+            return img
+        }
+        
+        // Return nil in case anything fails.
+        return nil
     }
 }
